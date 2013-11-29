ece321d1fc4eacba899e0fc9c9d80afea844be7a
Anton Kolesov <anton.kolesov@synopsys.com>
2013-11-29 12:29:53 +0400

    Prevent UI hang when auto terminating debug session
    Signed-off-by: Anton Kolesov <anton.kolesov@synopsys.com>

--- com.arc.embeddedcdt/src/com/arc/embeddedcdt/launch/LaunchTerminator.java
+++ com.arc.embeddedcdt/src/com/arc/embeddedcdt/launch/LaunchTerminator.java
@@ -13,6 +13,7 @@ package com.arc.embeddedcdt.launch;
 
 import org.eclipse.debug.core.DebugEvent;
 import org.eclipse.debug.core.DebugException;
+import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.IDebugEventSetListener;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.model.IProcess;
@@ -22,7 +23,61 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 
+/**
+ * Debug event listener to terminate debug sessions when required.
+ */
 public class LaunchTerminator implements IDebugEventSetListener {
+
+	/**
+	 * Runnable to terminate launch asynchronously.
+	 */
+	private class TerminateRunnable implements Runnable {
+
+		private ILaunch fLaunch;
+
+		public TerminateRunnable(ILaunch launch) {
+			this.fLaunch = launch;
+		}
+
+		public void run() {
+			try {
+				fLaunch.terminate();
+			} catch (DebugException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Show dialog and terminate launch.
+	 */
+	private class DialogRunnable implements Runnable {
+
+		private ILaunch fLaunch;
+		private IProcess fProcess;
+
+		public DialogRunnable(ILaunch launch, IProcess process) {
+			this.fLaunch = launch;
+			this.fProcess = process;
+		}
+
+		public void run() {
+			boolean terminate = true;
+			IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+			if (window != null) {
+				Shell parent = window.getShell();
+				terminate = MessageDialog.openQuestion(
+						parent,
+						"Child process failed",
+						"Process `" + fProcess.getLabel() + "' required for debugging has failed. Do you want to end debug session?"
+				);
+			}
+			if (terminate) {
+				DebugPlugin.getDefault().asyncExec(new TerminateRunnable(fLaunch));
+			}
+		}
+	}
+
 	public void handleDebugEvents(DebugEvent[] events) {
 		for (DebugEvent ev : events) {
 			int code = ev.getKind();
@@ -34,32 +89,12 @@ public class LaunchTerminator implements IDebugEventSetListener {
 						 p.getLabel() == Launch.ASHLING_PROCESS_LABEL ||
 						 p.getLabel().startsWith(Launch.GDB_PROCESS_LABEL) ) &&
 					 launch.canTerminate()) {
-
-					// Active help does not run on the UI thread, so we must use syncExec
-					Display.getDefault().syncExec(new Runnable() {
-						public void run() {
-							boolean terminate = true;
-							IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-							if (window != null) {
-								Shell parent = window.getShell();
-								terminate = MessageDialog.openQuestion(
-										parent,
-										"Child process failed",
-										"Process " + p.getLabel() + " requried for debugging has failed. Do you want to end debug session?"
-								);
-							}
-							if (terminate) {
-								try {
-									launch.terminate();
-								} catch (DebugException e) {
-									e.printStackTrace();
-								}
-							}
-						}
-					});
+					Display.getDefault().asyncExec(new DialogRunnable(launch, p));
+					/* If two processes are already terminated, then user
+					 * will get two dialogs without this return. */
+					return;
 				}
 			}
 		}
 	}
-
 }
