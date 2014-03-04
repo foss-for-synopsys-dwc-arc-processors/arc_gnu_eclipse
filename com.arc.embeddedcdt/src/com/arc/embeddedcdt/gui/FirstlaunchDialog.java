c16fe90c452260fdeae80056452d709a385dbf83
Yunlu Zhang <Yunlu.Zhang@synopsys.com>
2014-03-04 16:08:48 +0800

    Popup window class for Debug As and Run As
    Signed-off-by: Yunlu Zhang <Yunlu.Zhang@synopsys.com>

---
+++ com.arc.embeddedcdt/src/com/arc/embeddedcdt/gui/FirstlaunchDialog.java
@@ -0,0 +1,153 @@
+package com.arc.embeddedcdt.gui;
+
+import java.awt.Toolkit;
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Dialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.IViewReference;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.internal.Workbench;
+
+import com.arc.embeddedcdt.launch.Launch;
+
+public class FirstlaunchDialog extends Dialog {
+	public static String[] value= new String[2];;
+	public Combo fFirstlaunchPrgmArgumentsComCom;// this variable is for getting user's
+											// input COM port
+	protected Label fPrgmArgumentsLabelCom;
+
+	/**
+	 * @param parent
+	 */
+	public FirstlaunchDialog(Shell parent) {
+		super(parent);
+	}
+
+	/**
+	 * @param parent
+	 * @param style
+	 */
+	public FirstlaunchDialog(Shell parent, int style) {
+		super(parent, style);
+	}
+
+	/**
+	 * Makes the dialog visible.
+	 * 
+	 * @return
+	 */
+	public String[] open() {
+		Shell parent = getParent();
+		final Shell shell = new Shell(parent, SWT.TITLE | SWT.BORDER
+				| SWT.APPLICATION_MODAL);
+		shell.setLocation(Display.getCurrent().getClientArea().width / 2
+				- shell.getShell().getSize().x / 2, Display.getCurrent()
+				.getClientArea().height / 2 - shell.getSize().y / 2);
+
+		shell.setText("Before Debug as Configuration");
+
+		shell.setLayout(new GridLayout(2, true));
+
+		Label label = new Label(shell, SWT.NULL);
+		label.setText("ARC GDB Server:");
+
+		Combo fPrgmArgumentsComboInit = new Combo(shell, SWT.SINGLE
+				| SWT.BORDER);
+		fPrgmArgumentsComboInit.add("JTAG via OpenOCD");
+		fPrgmArgumentsComboInit.add("JTAG via Ashling");
+		fPrgmArgumentsComboInit.add("nSIM");
+
+		fPrgmArgumentsLabelCom = new Label(shell, SWT.NULL);
+		fPrgmArgumentsLabelCom.setText("COM  Ports:"); //$NON-NLS-1$
+
+		fFirstlaunchPrgmArgumentsComCom = new Combo(shell, SWT.SINGLE | SWT.BORDER);
+
+		List COM = Launch.COMserialport();
+		for (int ii = 0; ii < COM.size(); ii++) {
+			String currentcom = (String) COM.get(ii);
+			fFirstlaunchPrgmArgumentsComCom.add(currentcom);
+		}
+
+		fFirstlaunchPrgmArgumentsComCom.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent evt) {
+				Combo combo = (Combo) evt.widget;
+				value[1] = combo.getText();
+			}
+
+		});
+
+		fPrgmArgumentsComboInit.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent evt) {
+				Combo combo = (Combo) evt.widget;
+				value[0] = combo.getText();
+				String gdbserver = combo.getText();
+				if (gdbserver.equalsIgnoreCase("nSIM")) {
+					fFirstlaunchPrgmArgumentsComCom.setVisible(false);
+					fPrgmArgumentsLabelCom.setVisible(false);
+				}
+				else {
+					fFirstlaunchPrgmArgumentsComCom.setVisible(true);
+					fPrgmArgumentsLabelCom.setVisible(true);
+				}
+			}
+		});
+		final Button buttonOK = new Button(shell, SWT.PUSH);
+		buttonOK.setText("     Ok     ");
+		buttonOK.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_END));
+		Button buttonCancel = new Button(shell, SWT.PUSH);
+		buttonCancel.setText("Cancel");
+
+		buttonOK.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event event) {
+				shell.dispose();
+			}
+		});
+
+		buttonCancel.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event event) {
+				value[0] = null;
+				value[1] = null;
+				shell.dispose();
+			}
+		});
+
+		shell.addListener(SWT.Traverse, new Listener() {
+			public void handleEvent(Event event) {
+				if (event.detail == SWT.TRAVERSE_ESCAPE)
+					event.doit = false;
+			}
+		});
+		fFirstlaunchPrgmArgumentsComCom.setText("");
+		fPrgmArgumentsComboInit.setText("");
+		shell.pack();
+		shell.open();
+
+		Display display = parent.getDisplay();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
+		}
+		System.out.println("gdbserver: \""+value[0]+"\" COM serial port: \""+value[1]+"\"");
+		return value;
+
+	}
+
+	public static void main(String[] args) {
+		Shell shell = new Shell();
+		FirstlaunchDialog dialog = new FirstlaunchDialog(shell);
+		dialog.open();
+	}
+}
