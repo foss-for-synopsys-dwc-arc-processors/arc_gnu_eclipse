cf21f6273f5ea8895594cea220dce69b6cb9c470
Yunlu Zhang <Yunlu.Zhang@synopsys.com>
2014-04-18 16:23:10 +0800

    Create separate toolchains for each core (HS/EM)
    This batch has both EM&HS toolchain in complier plugin, so now each config
    will have different toolchain, and each core have different options. In
    current toolchain, only corrent -mcpu will be showed up.
    
    Undone problem:
    1: Can change EM to HS, but get error when changing back
    2: Cannot complie HS project
    3: Need to change "Empty project for EH/HS" 's default toolchain
    
    Signed-off-by: Yunlu Zhang <Yunlu.Zhang@synopsys.com>

--- org.eclipse.cdt.cross.arc.gnu/src/com/arc/cdt/toolchain/arc/ArcOptionEnablementManager.java
+++ org.eclipse.cdt.cross.arc.gnu/src/com/arc/cdt/toolchain/arc/ArcOptionEnablementManager.java
@@ -41,10 +41,10 @@ public class ArcOptionEnablementManager extends OptionEnablementManager {
     };
     
     private static String ARCV2EM_INVISIBLED[]= {
-    	"org.eclipse.cdt.cross.arc.gnu.windows.option.target.fpuhs",
-    	"org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.fpuhs",
-    	"org.eclipse.cdt.cross.arc.gnu.linux.option.target.fpuhs",
-    	"org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.fpuhs",
+    	"org.eclipse.cdt.cross.arc.gnu.windows.option.target.fpuhs.hs",
+    	"org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.fpuhs.hs",
+    	"org.eclipse.cdt.cross.arc.gnu.linux.option.target.fpuhs.hs",
+    	"org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.fpuhs.hs",
     	
     	
     	"org.eclipse.cdt.cross.arc.gnu.windows.option.target.mpyhs",
@@ -97,44 +97,44 @@ public class ArcOptionEnablementManager extends OptionEnablementManager {
     };
     private static String ARCV2HS_DISABLED[] = {
 
-        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.ea",//Customized for ARC GNU ea   
-        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.ea",//Customized for ARC GNU ea  
-        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.codedensity", //Customized for ARC GNU windows_codedensity
-	    "org.eclipse.cdt.cross.arc.gnu.linux.option.target.codedensity", //Customized for ARC GNU linux_codedensity
-        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.swap",//Customized for ARC GNU windows_swap
-        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.swap",//Customized for ARC GNU linux_swap
-        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.barrelshifter",//
-        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.barrelshifter",//
+        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.ea.hs",//Customized for ARC GNU ea   
+        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.ea.hs",//Customized for ARC GNU ea  
+        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.codedensity.hs", //Customized for ARC GNU windows_codedensity
+	    "org.eclipse.cdt.cross.arc.gnu.linux.option.target.codedensity.hs", //Customized for ARC GNU linux_codedensity
+        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.swap.hs",//Customized for ARC GNU windows_swap
+        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.swap.hs",//Customized for ARC GNU linux_swap
+        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.barrelshifter.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.barrelshifter.hs",//
 
-        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.shiftassist",//
-        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.shiftassist",//
-        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.normalize",//
-        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.normalize",
+        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.shiftassist.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.shiftassist.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.normalize.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.normalize.hs",
         
-	    "org.eclipse.cdt.cross.arc.gnu.windows.option.target.fpi",//
-        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.fpi",//
-        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.mno-dpfp-lrsr",//
-        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.mno-dpfp-lrsr",//
+	    "org.eclipse.cdt.cross.arc.gnu.windows.option.target.fpi.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.fpi.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.mno-dpfp-lrsr.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.mno-dpfp-lrsr.hs",//
      
         
-        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.ea",//Customized for ARC GNU ea    big-en
-        "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.ea",//Customized for ARC GNU ea  big-en
-        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.codedensity", //Customized for ARC GNU windows_codedensity big-en
-	    "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.codedensity", //Customized for ARC GNU linux_codedensity big-en
-        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.swap",//Customized for ARC GNU windows_swap big-en
-        "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.swap",//Customized for ARC GNU linux_swap big-en
-        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.barrelshifter",//
-        "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.barrelshifter",//
+        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.ea.hs",//Customized for ARC GNU ea    big-en
+        "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.ea.hs",//Customized for ARC GNU ea  big-en
+        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.codedensity.hs", //Customized for ARC GNU windows_codedensity big-en
+	    "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.codedensity.hs", //Customized for ARC GNU linux_codedensity big-en
+        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.swap.hs",//Customized for ARC GNU windows_swap big-en
+        "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.swap.hs",//Customized for ARC GNU linux_swap big-en
+        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.barrelshifter.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.barrelshifter.hs",//
 
-        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.shiftassist",//
-        "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.shiftassist",//
-        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.normalize",//
+        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.shiftassist.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.shiftassist.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.normalize.hs",//
         "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.normalize",
         
-	    "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.fpi",//
-        "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.fpi",//
-        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.mno-dpfp-lrsr",//
-        "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.mno-dpfp-lrsr",//
+	    "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.fpi.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.fpi.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.mno-dpfp-lrsr.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.mno-dpfp-lrsr.hs",//
         
         
     };
@@ -143,28 +143,28 @@ public class ArcOptionEnablementManager extends OptionEnablementManager {
     private static String[]  ARCV2HS_DEFAULT = {
 
         // HS default value to be true
-        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.codedensity", //Customized for ARC GNU windows_codedensity
-	    "org.eclipse.cdt.cross.arc.gnu.linux.option.target.codedensity", //Customized for ARC GNU linux_codedensity
-        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.swap",//Customized for ARC GNU windows_swap
-        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.swap",//Customized for ARC GNU linux_swap
-        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.barrelshifter",//
-        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.barrelshifter",//
-        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.shiftassist",//
-        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.shiftassist",//
-        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.normalize",//
-        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.normalize",
+        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.codedensity.hs", //Customized for ARC GNU windows_codedensity
+	    "org.eclipse.cdt.cross.arc.gnu.linux.option.target.codedensity.hs", //Customized for ARC GNU linux_codedensity
+        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.swap.hs",//Customized for ARC GNU windows_swap
+        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.swap.hs",//Customized for ARC GNU linux_swap
+        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.barrelshifter.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.barrelshifter.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.shiftassist.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.shiftassist.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.windows.option.target.normalize.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.linux.option.target.normalize.hs",
         
         // HS default value for big-en to be true
-        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.codedensity", //Customized for ARC GNU windows_codedensity
-	    "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.codedensity", //Customized for ARC GNU linux_codedensity
-        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.swap",//Customized for ARC GNU windows_swap
-        "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.swap",//Customized for ARC GNU linux_swap
-        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.barrelshifter",//
-        "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.barrelshifter",//
-        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.shiftassist",//
-        "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.shiftassist",//
-        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.normalize",//
-        "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.normalize",
+        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.codedensity.hs", //Customized for ARC GNU windows_codedensity
+	    "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.codedensity.hs", //Customized for ARC GNU linux_codedensity
+        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.swap.hs",//Customized for ARC GNU windows_swap
+        "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.swap.hs",//Customized for ARC GNU linux_swap
+        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.barrelshifter.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.barrelshifter.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.shiftassist.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.shiftassist.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.eb.windows.option.target.normalize.hs",//
+        "org.eclipse.cdt.cross.arc.gnu.eb.linux.option.target.normalize.hs",
     };
     
     private static Set<String> DISABLED_FOR_ARC7 = new HashSet<String>(Arrays.asList(ARC7_DISABLED));
