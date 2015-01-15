072751ab31c105af4384414e9e8f8aa425f3e5e4
Anton Kolesov <Anton.Kolesov@synopsys.com>
2015-01-15 18:08:23 +0300

    6100015885 Update copyright years
    Signed-off-by: Anton Kolesov <Anton.Kolesov@synopsys.com>

--- com.arc.embeddedcdt/src/com/arc/embeddedcdt/launch/LaunchShortcut.java
+++ com.arc.embeddedcdt/src/com/arc/embeddedcdt/launch/LaunchShortcut.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2014 Synopsys, Inc.  All rights reserved.
+ * Copyright (c) 2013, 2015 Synopsys, Inc.  All rights reserved.
  * This program and the accompanying materials
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
