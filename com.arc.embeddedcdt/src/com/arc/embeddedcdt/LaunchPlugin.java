5aa198b055e9801c8ef2386e3d6a6f17ebb13bb0
Anna Pologova <anna.pologova@synopsys.com>
2016-06-08 17:30:25 +0300

    Fix formatting and delete comments in LaunchPlugin
    Signed-off-by: Anna Pologova <anna.pologova@synopsys.com>

--- com.arc.embeddedcdt/src/com/arc/embeddedcdt/LaunchPlugin.java
+++ com.arc.embeddedcdt/src/com/arc/embeddedcdt/LaunchPlugin.java
@@ -32,88 +32,79 @@ import org.osgi.framework.BundleContext;
  * The main plugin class to be used in the desktop.
  */
 public class LaunchPlugin extends AbstractUIPlugin {
-	public static final String PLUGIN_ID = "com.arc.embeddedcdt"; //$NON-NLS-1$
+    public static final String PLUGIN_ID = "com.arc.embeddedcdt"; //$NON-NLS-1$
 
-	//The shared instance.
-	private static LaunchPlugin plugin;
-	//Resource bundle.
-//	private ResourceBundle resourceBundle;
-	
-	/**
-	 * The constructor.
-	 */
-	public LaunchPlugin() {
-		super();
-		plugin = this;
-//		try {
-//			resourceBundle = ResourceBundle.getBundle("com.arc.embeddedcdt.PluginResources");
-//		} catch (MissingResourceException x) {
-//			resourceBundle = null;
-//		}
-	}
+    // The shared instance.
+    private static LaunchPlugin plugin;
 
-	/**
-	 * This method is called upon plug-in activation
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-	}
+    /**
+     * The constructor.
+     */
+    public LaunchPlugin() {
+        super();
+        plugin = this;
+    }
 
-	/**
-	 * This method is called when the plug-in is stopped
-	 */
-	public void stop(BundleContext context) throws Exception {
-		super.stop(context);
-	}
+    /**
+     * This method is called upon plug-in activation
+     */
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+    }
 
-	/**
-	 * Returns the shared instance.
-	 */
-	public static LaunchPlugin getDefault() {
-		return plugin;
-	}
+    /**
+     * This method is called when the plug-in is stopped
+     */
+    public void stop(BundleContext context) throws Exception {
+        super.stop(context);
+    }
 
-	/**
-	 * Returns the string from the plugin's resource bundle,
-	 * or 'key' if not found.
-	 */
-	public static String getResourceString(String key) {
-		ResourceBundle bundle = LaunchPlugin.getDefault().getResourceBundle();
-		try {
-			return (bundle != null) ? bundle.getString(key) : key;
-		} catch (MissingResourceException e) {
-			return key;
-		}
-	}
+    /**
+     * Returns the shared instance.
+     */
+    public static LaunchPlugin getDefault() {
+        return plugin;
+    }
 
-	/**
-	 * Returns the plugin's resource bundle,
-	 */
-	public ResourceBundle getResourceBundle() {
-		return Platform.getResourceBundle(getBundle()) ;
-	}
+    /**
+     * Returns the string from the plugin's resource bundle, or 'key' if not found.
+     */
+    public static String getResourceString(String key) {
+        ResourceBundle bundle = LaunchPlugin.getDefault().getResourceBundle();
+        try {
+            return (bundle != null) ? bundle.getString(key) : key;
+        } catch (MissingResourceException e) {
+            return key;
+        }
+    }
 
+    /**
+     * Returns the plugin's resource bundle,
+     */
+    public ResourceBundle getResourceBundle() {
+        return Platform.getResourceBundle(getBundle());
+    }
 
-	/**
-	 * Convenience method which returns the unique identifier of this plugin.
-	 */
-	public static String getUniqueIdentifier() {
-		if (getDefault() == null) {
-			// If the default instance is not yet initialized,
-			// return a static identifier. This identifier must
-			// match the plugin id defined in plugin.xml
-			return PLUGIN_ID;
-		}
-		return getDefault().getBundle().getSymbolicName();
-	}
+    /**
+     * Convenience method which returns the unique identifier of this plugin.
+     */
+    public static String getUniqueIdentifier() {
+        if (getDefault() == null) {
+            // If the default instance is not yet initialized,
+            // return a static identifier. This identifier must
+            // match the plugin id defined in plugin.xml
+            return PLUGIN_ID;
+        }
+        return getDefault().getBundle().getSymbolicName();
+    }
 
-	/**
-	 * Helper message for exception handling.
-	 *
-	 * @param e Exception to log/
-	 */
-	public static void log(CoreException e)
-	{
-		StatusManager.getManager().handle(e, LaunchPlugin.getUniqueIdentifier());
-	}
+    /**
+     * Helper message for exception handling.
+     *
+     * @param e
+     *            Exception to log/
+     */
+    public static void log(CoreException e) {
+        StatusManager.getManager().handle(e, LaunchPlugin.getUniqueIdentifier());
+    }
 }
