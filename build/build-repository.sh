3b415b442a5f4dd36d0ff905c27e2626f1d3d07d
alexander <polyakov@synopsys.com>
2017-09-06 15:09:56 +0300

    Change used version of java
    Changing required since old Java version isn't available on some machines.

--- build/build-repository.sh
+++ build/build-repository.sh
@@ -7,7 +7,7 @@ ECLIPSE_P2_REPO=$PREREQ/eclipse/$ECLIPSE_VERSION/p2_repository
 MAVEN=$PREREQ/apache-maven-3.3.9/bin/mvn
 MAVEN_LOCAL_REPO=$PREREQ/eclipse/$ECLIPSE_VERSION/maven_local_repo
 
-JAVA_HOME=/depot/java-1.8.0_91
+JAVA_HOME=/depot/java-1.8.0_131
 export PATH=$JAVA_HOME/bin:$PATH
 
 $MAVEN \
