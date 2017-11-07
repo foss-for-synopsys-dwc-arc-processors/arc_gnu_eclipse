a84262705641c176d030257b4cdc59af7402fe71
MrsSima <darya@synopsys.com>
2017-11-07 14:01:27 +0300

    Update Eclipse version in build/build-repository.sh

--- build/build-repository.sh
+++ build/build-repository.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 PREREQ=/u/arcgnu_verif/prerequisites
-ECLIPSE_VERSION=neon-4.6
-ECLIPSE_P2_REPO=$PREREQ/eclipse/$ECLIPSE_VERSION/p2_repository
+ECLIPSE_VERSION=oxygen-4.7
 
 MAVEN=$PREREQ/apache-maven-3.3.9/bin/mvn
 MAVEN_LOCAL_REPO=$PREREQ/eclipse/$ECLIPSE_VERSION/maven_local_repo
@@ -12,5 +11,4 @@ export PATH=$JAVA_HOME/bin:$PATH
 
 $MAVEN \
 	-Dmaven.repo.local=$MAVEN_LOCAL_REPO \
-	-Declipse.repository=file:$ECLIPSE_P2_REPO \
 	-o clean install
