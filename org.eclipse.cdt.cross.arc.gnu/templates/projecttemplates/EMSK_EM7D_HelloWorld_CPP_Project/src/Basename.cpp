b7b8269fe2576fc62ee9e0b368fcc4ecc1ef9bbf
MrsSima <darya@synopsys.com>
2018-05-24 20:52:12 +0300

    Update Hello World projects for EMSK
    Now there are projects for ARC EM7D,EM9D and EM11D separately.
    Specs files are using as default (EM9D specs file for EM9D and EM7D,
    EM11D for EM11D). lds files are removed as unnecessary.

---
+++ org.eclipse.cdt.cross.arc.gnu/templates/projecttemplates/EMSK_EM7D_HelloWorld_CPP_Project/src/Basename.cpp
@@ -0,0 +1,15 @@
+/*
+ Name        : $(baseName).c
+ Author      : $(author)
+ Version     : 1.0
+ Copyright   : $(copyright)
+ Description : Print a greeting on UART output and exit.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("$(messagearc)\n\r");
+	return 0;
+}
