64a8cdce19cea1b79e10f705677d33dd3902677a
Anton Kolesov <Anton.Kolesov@synopsys.com>
2018-11-27 17:32:21 +0300

    elf32, uclibc: Fix few raw type references

--- org.eclipse.cdt.cross.arc.gnu.uclibc/src/org/eclipse/cdt/cross/arc/gnu/uclibc/scannerconfig/ARCGnuWinScannerInfoCollector.java
+++ org.eclipse.cdt.cross.arc.gnu.uclibc/src/org/eclipse/cdt/cross/arc/gnu/uclibc/scannerconfig/ARCGnuWinScannerInfoCollector.java
@@ -1,53 +1,53 @@
-/*    */ package org.eclipse.cdt.cross.arc.gnu.uclibc.scannerconfig;
-/*    */ 
-/*    */ import java.util.Iterator;
-/*    */ import java.util.List;
-/*    */ import java.util.Map;
-/*    */ import org.eclipse.cdt.make.core.scannerconfig.IScannerInfoCollector3;
-/*    */ import org.eclipse.cdt.make.core.scannerconfig.InfoContext;
-/*    */ import org.eclipse.cdt.make.core.scannerconfig.ScannerInfoTypes;
-/*    */ import org.eclipse.cdt.make.internal.core.scannerconfig.util.CygpathTranslator;
-/*    */ import org.eclipse.cdt.make.internal.core.scannerconfig2.PerProjectSICollector;
-/*    */ import org.eclipse.cdt.managedbuilder.scannerconfig.IManagedScannerInfoCollector;
-/*    */ import org.eclipse.core.resources.IProject;
-/*    */ 
-/*    */ public class ARCGnuWinScannerInfoCollector extends PerProjectSICollector
-/*    */   implements IScannerInfoCollector3, IManagedScannerInfoCollector
-/*    */ {
-/*    */   private IProject m_oProject;
-/*    */ 
-/*    */   public void contributeToScannerConfig(Object oResource, Map oScannerInfo)
-/*    */   {
-/* 28 */     List oIncludes = (List)oScannerInfo.get(ScannerInfoTypes.INCLUDE_PATHS);
-/*    */ 
-/* 32 */     List oTranslatedIncludes = CygpathTranslator.translateIncludePaths(this.m_oProject, oIncludes);
-/* 33 */     Iterator oPathIter = oTranslatedIncludes.listIterator();
-/* 34 */     while (oPathIter.hasNext()) {
-/* 35 */       String sConvertedPath = (String)oPathIter.next();
-/*    */ 
-/* 37 */       if (sConvertedPath.startsWith("/")) {
-/* 38 */         oPathIter.remove();
-/*    */       }
-/*    */ 
-/*    */     }
-/*    */ 
-/* 44 */     oScannerInfo.put(ScannerInfoTypes.INCLUDE_PATHS, oTranslatedIncludes);
-/*    */ 
-/* 55 */     super.contributeToScannerConfig(oResource, oScannerInfo);
-/*    */   }
-/*    */ 
-/*    */   public void setProject(IProject oProject) {
-/* 59 */     this.m_oProject = oProject;
-/* 60 */     super.setProject(oProject);
-/*    */   }
-/*    */ 
-/*    */   public void setInfoContext(InfoContext oContext) {
-/* 64 */     this.m_oProject = oContext.getProject();
-/* 65 */     super.setInfoContext(oContext);
-/*    */   }
-/*    */ }
+package org.eclipse.cdt.cross.arc.gnu.uclibc.scannerconfig;
 
-/* Location:           C:\Users\yunluz\Desktop\plugins\bin\
- * Qualified Name:     org.eclipse.cdt.cross.arm.gnu.scannerconfig.ARMGnuWinScannerInfoCollector
- * JD-Core Version:    0.6.2
- */
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.cdt.make.core.scannerconfig.IScannerInfoCollector3;
+import org.eclipse.cdt.make.core.scannerconfig.InfoContext;
+import org.eclipse.cdt.make.core.scannerconfig.ScannerInfoTypes;
+import org.eclipse.cdt.make.internal.core.scannerconfig.util.CygpathTranslator;
+import org.eclipse.cdt.make.internal.core.scannerconfig2.PerProjectSICollector;
+import org.eclipse.cdt.managedbuilder.scannerconfig.IManagedScannerInfoCollector;
+import org.eclipse.core.resources.IProject;
+
+public class ARCGnuWinScannerInfoCollector extends PerProjectSICollector
+        implements IScannerInfoCollector3, IManagedScannerInfoCollector {
+    private IProject m_oProject;
+
+    @Override
+    public void contributeToScannerConfig(
+            Object oResource,
+            @SuppressWarnings("rawtypes") Map oScannerInfo) {
+        List<String> oIncludes = (List<String>) oScannerInfo.get(ScannerInfoTypes.INCLUDE_PATHS);
+
+        List<String> oTranslatedIncludes = CygpathTranslator.translateIncludePaths(this.m_oProject,
+                oIncludes);
+        Iterator<String> oPathIter = oTranslatedIncludes.listIterator();
+        while (oPathIter.hasNext()) {
+            String sConvertedPath = oPathIter.next();
+
+            if (sConvertedPath.startsWith("/")) {
+                oPathIter.remove();
+            }
+
+        }
+
+        oScannerInfo.put(ScannerInfoTypes.INCLUDE_PATHS, oTranslatedIncludes);
+
+        super.contributeToScannerConfig(oResource, oScannerInfo);
+    }
+
+    @Override
+    public void setProject(IProject oProject) {
+        this.m_oProject = oProject;
+        super.setProject(oProject);
+    }
+
+    @Override
+    public void setInfoContext(InfoContext oContext) {
+        this.m_oProject = oContext.getProject();
+        super.setInfoContext(oContext);
+    }
+}
--- org.eclipse.cdt.cross.arc.gnu/src/org/eclipse/cdt/cross/arc/gnu/scannerconfig/ARCGnuWinScannerInfoCollector.java
+++ org.eclipse.cdt.cross.arc.gnu/src/org/eclipse/cdt/cross/arc/gnu/scannerconfig/ARCGnuWinScannerInfoCollector.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
-* This program and the accompanying materials 
-* are made available under the terms of the Common Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/cpl-v10.html
-* 
-* Contributors:
-*     Synopsys, Inc. - ARC GNU Toolchain support
-*******************************************************************************/
+ * This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *
+ * Contributors:
+ *     Synopsys, Inc. - ARC GNU Toolchain support
+ *******************************************************************************/
 
 package org.eclipse.cdt.cross.arc.gnu.scannerconfig;
 
@@ -22,38 +22,43 @@ import org.eclipse.cdt.make.internal.core.scannerconfig2.PerProjectSICollector;
 import org.eclipse.cdt.managedbuilder.scannerconfig.IManagedScannerInfoCollector;
 import org.eclipse.core.resources.IProject;
 
- public class ARCGnuWinScannerInfoCollector extends PerProjectSICollector
-   implements IScannerInfoCollector3, IManagedScannerInfoCollector
- {
-   private IProject m_oProject;
- 
-   public void contributeToScannerConfig(Object oResource, Map oScannerInfo)
-   {
-     List oIncludes = (List)oScannerInfo.get(ScannerInfoTypes.INCLUDE_PATHS);
- 
-     List oTranslatedIncludes = CygpathTranslator.translateIncludePaths(this.m_oProject, oIncludes);
-     Iterator oPathIter = oTranslatedIncludes.listIterator();
-     while (oPathIter.hasNext()) {
-       String sConvertedPath = (String)oPathIter.next();
- 
-       if (sConvertedPath.startsWith("/")) {
-         oPathIter.remove();
-       }
- 
-     }
- 
-     oScannerInfo.put(ScannerInfoTypes.INCLUDE_PATHS, oTranslatedIncludes);
- 
-     super.contributeToScannerConfig(oResource, oScannerInfo);
-   }
- 
-   public void setProject(IProject oProject) {
-     this.m_oProject = oProject;
-     super.setProject(oProject);
-   }
- 
-   public void setInfoContext(InfoContext oContext) {
-     this.m_oProject = oContext.getProject();
-     super.setInfoContext(oContext);
-   }
- }
+public class ARCGnuWinScannerInfoCollector extends PerProjectSICollector
+implements IScannerInfoCollector3, IManagedScannerInfoCollector
+{
+    private IProject m_oProject;
+
+    @Override
+    public void contributeToScannerConfig(Object oResource,
+            @SuppressWarnings("rawtypes") Map oScannerInfo)
+    {
+        List<String> oIncludes = (List<String>) oScannerInfo.get(ScannerInfoTypes.INCLUDE_PATHS);
+
+        List<String> oTranslatedIncludes = CygpathTranslator.translateIncludePaths(this.m_oProject,
+                oIncludes);
+        Iterator<String> oPathIter = oTranslatedIncludes.listIterator();
+        while (oPathIter.hasNext()) {
+            String sConvertedPath = oPathIter.next();
+
+            if (sConvertedPath.startsWith("/")) {
+                oPathIter.remove();
+            }
+
+        }
+
+        oScannerInfo.put(ScannerInfoTypes.INCLUDE_PATHS, oTranslatedIncludes);
+
+        super.contributeToScannerConfig(oResource, oScannerInfo);
+    }
+
+    @Override
+    public void setProject(IProject oProject) {
+        this.m_oProject = oProject;
+        super.setProject(oProject);
+    }
+
+    @Override
+    public void setInfoContext(InfoContext oContext) {
+        this.m_oProject = oContext.getProject();
+        super.setInfoContext(oContext);
+    }
+}
