2d48513443319c9b6addf463796ed1081d28cec9
Yuriy Kolerov <ykolerov@synopsys.com>
2023-09-01 17:40:09 +0400

    Update all specs properties and all projects
    Newlib 2023.09 introduces a completely new set of specs files:
    HSDK, EM SDP (ICCM/DCCM and RAM-based), EMSK (ICCM/DCCM and
    RAM-based), IoTDK.
    
    Here is a list of updates:
    
    * By default, each project uses a corresponding specs file for
      the particular board. TCF file is used only for options and
      board's headers.
    * A project for each board is shipped with TCF file.
    * All C++ projects use RAM-based specs file to make it
      possible to fit runtime in memory. It was impossible to
      build even simple C++ applications earlier.

--- gnu.elf/templates/emsk_em7d_hello_world_cpp/src/main.cpp
+++ gnu.elf/templates/emsk_em7d_hello_world_cpp/src/main.cpp
@@ -1,9 +1,11 @@
 /* Print a greeting on UART output and exit. */
 
-#include <stdio.h>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-	printf("$(messagearc)\n\r");
+	cout << "$(messagearc)\n\r" << endl;
 	return 0;
 }
